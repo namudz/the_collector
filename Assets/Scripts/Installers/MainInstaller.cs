66055b00aeb1749946affb2a72aaaf0a2f043f10
Manu Diaz <mdiaz.sanguino@gmail.com>
2021-01-25 22:25:59 +0100

    Added MainScreen view & Installer + Updated Main scene + Created empty Game scene

---
+++ Assets/Scripts/Installers/MainInstaller.cs
@@ -0,0 +1,29 @@
+﻿using EventDispatcher;
+using SceneLoader;
+
+namespace Installers
+{
+    public class MainInstaller
+    {
+        private readonly MainViews _mainViews;
+        private ISceneLoader _sceneLoader;
+        private IEventDispatcher _eventDispatcher;
+
+        public MainInstaller(MainViews mainViews)
+        {
+            _mainViews = mainViews;
+        }
+
+        public void InjectDependencies()
+        {
+            InitializeDependencies();
+            _mainViews.MainScreenView.InjectDependencies(_sceneLoader);
+        }
+
+        private void InitializeDependencies()
+        {
+            _eventDispatcher = new EventDispatcher.EventDispatcher();
+            _sceneLoader = new SceneLoader.SceneLoader(_eventDispatcher);
+        }
+    }
+}
