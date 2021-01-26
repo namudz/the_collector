a4a2be948216dc345c5e97196ddb55821125136d
Manu Diaz <mdiaz.sanguino@gmail.com>
2021-01-26 18:37:38 +0100

    Created ISpawner & HeroSpawner class + Updated scene and hero prefab + Updated game images

--- Assets/Scripts/Hero/Spawner/HeroSpawner.cs
+++ Assets/Scripts/Hero/Spawner/HeroSpawner.cs
@@ -2,15 +2,13 @@ using UnityEngine;
 
 public class HeroSpawner : MonoBehaviour, ISpawner
 {
+    [SerializeField] private Transform _spawnPoint;
     [SerializeField] private GameObject _hero;
-
-    private void Start() 
-    {
-        Spawn();   
-    }
 
     public void Spawn()
     {
+        // TODO - Get the reference of the object to spawn differently
+        _hero.transform.position = _spawnPoint.position;
         _hero.SetActive(true);
     }
 }
