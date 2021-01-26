f9604c5f6cbd1571b59af21354d22b8a0fdeb2d6
Manu Diaz <mdiaz.sanguino@gmail.com>
2021-01-26 20:47:18 +0100

    Updated ISceneLoader to load maze scenes

--- Assets/Scripts/SceneLoader/SceneLoader.cs
+++ Assets/Scripts/SceneLoader/SceneLoader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using EventDispatcher;
 using UnityEngine.SceneManagement;
 
@@ -11,6 +12,11 @@ namespace SceneLoader
             { SceneConstants.Scene.MainMenu, "0_MainScene" },
             { SceneConstants.Scene.Game, "1_GameScene" }
         };
+        
+        private readonly Dictionary<SceneConstants.Mazes, string> _mazes = new Dictionary<SceneConstants.Mazes,string>
+        {
+            { SceneConstants.Mazes.Level_1, "Maze_1" }
+        };
 
         private readonly IEventDispatcher _eventDispatcher;
         private SceneConstants.Scene _sceneLoaded;
@@ -19,17 +25,25 @@ namespace SceneLoader
         {
             _eventDispatcher = eventDispatcher;
         }
-
+        
         public void LoadScene(SceneConstants.Scene scene, LoadSceneMode mode = LoadSceneMode.Single)
         {
             _sceneLoaded = scene;
-            SceneManager.sceneLoaded += (arg0, sceneMode) =>
-            {
-                DispatchSceneLoadedEvent();
-            };
-            SceneManager.LoadSceneAsync(_scenes[scene], mode);
+            LoadUnityScene(_scenes[scene], mode, DispatchSceneLoadedEvent);
         }
         
+        public void LoadMazeScene(SceneConstants.Mazes mazeScene, Action onComplete)
+        {
+            //_sceneLoaded = mazeScene;
+            LoadUnityScene(_mazes[mazeScene], LoadSceneMode.Additive, onComplete);
+        }
+
+        private void LoadUnityScene(string sceneName, LoadSceneMode mode, Action onComplete)
+        {
+            SceneManager.sceneLoaded += (arg0, sceneMode) => { onComplete?.Invoke(); };
+            SceneManager.LoadSceneAsync(sceneName, mode);
+        }
+
         private void DispatchSceneLoadedEvent()
         {
             _eventDispatcher.Dispatch(new SceneLoadedSignal(_sceneLoaded));
