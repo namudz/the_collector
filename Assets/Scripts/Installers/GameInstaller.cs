1b0b0dc53e0551d19ccd6b4e239168f91dbd7ec1
Manu Diaz <mdiaz.sanguino@gmail.com>
2021-01-26 18:36:03 +0100

    Created GameInstaller + Created IGame & Game classes

---
+++ Assets/Scripts/Installers/GameInstaller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Installers
+{
+    public class GameInstaller
+    {
+        public IGame Game { get; private set; }
+
+        private GameInstaller()
+        {
+            Game = new Game();
+        }
+        public GameInstaller(ISpawner heroSpawner, IEnumerable<ISpawner> collectibleSpawners): this()
+        {
+            Game.InjectSpawners(heroSpawner, collectibleSpawners);
+        }
+
+        public void LoadGame()
+        {
+            Game.Load();
+        }
+    }
+}
