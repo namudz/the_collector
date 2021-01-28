d5e56f76b34b97d62e323beebbcaf9b069ae5ea7
Manu Diaz <mdiaz.sanguino@gmail.com>
2021-01-28 18:49:40 +0100

    Added interface to handle user input + Added HeroCollisionsController + Added Hero installer

---
+++ Assets/Scripts/InputHandler/IInputHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace InputHandler
+{
+    public interface IInputHandler
+    {
+        event Action OnTap;
+        void HandleInput();
+    }
+}
---
+++ Assets/Scripts/InputHandler/InputMobileHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace InputHandler
+{
+    public class InputMobileHandler : AbstractInputHandler
+    {
+        public override void HandleInput()
+        {
+            if(!IsValidTouch())
+            {
+                return;
+            }
+
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                LaunchOnTap();
+            }
+        }
+        
+        private bool IsValidTouch()
+        {
+            return Input.touchCount > 0 & !EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId);
+        }
+
+    }
+}
