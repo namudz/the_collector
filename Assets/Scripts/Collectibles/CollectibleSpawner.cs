7fdf29278193b4850ceacb6d0480ae0de5e53e7e
Manu Diaz <mdiaz.sanguino@gmail.com>
2021-01-28 20:02:36 +0100

    Added Spawner script folder + Added collectible spawner + Added collectible class & scriptable config object

---
+++ Assets/Scripts/Collectibles/CollectibleSpawner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Collectibles
+{
+    public class CollectibleSpawner : Spawner
+    {
+        [Header("Components")]
+        [SerializeField] private GameObject _collectiblePrefab;
+        
+        public override void Spawn()
+        {
+            var instance = Instantiate(_collectiblePrefab, _spawnPoint.position, Quaternion.identity, transform);
+            instance.SetActive(true);
+        }
+    }
+}
