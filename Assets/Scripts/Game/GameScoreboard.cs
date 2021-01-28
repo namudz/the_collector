79c9a04454af7cbbf2e3c59d7de772913eacea4e
Manu Diaz <mdiaz.sanguino@gmail.com>
2021-01-28 20:55:02 +0100

    Added IGameScoreboard

---
+++ Assets/Scripts/Game/GameScoreboard.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+public class GameScoreboard : IGameScoreboard
+{
+    public event Action<int> OnScoreUpdated;
+    
+    private static GameScoreboard _instance;
+
+    public static GameScoreboard Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameScoreboard();
+            }
+            return _instance;
+        }
+    }
+    public int CurrentScore { get; private set; }
+
+    public GameScoreboard()
+    {
+        CurrentScore = 0;
+    }
+    
+    public void AddScore(int score)
+    {
+        CurrentScore += score;
+        PrintScore();
+    }
+
+    public void AddScore(float percIncrement)
+    {
+        CurrentScore += Mathf.RoundToInt(CurrentScore * percIncrement);
+    }
+
+    private void PrintScore()
+    {
+        OnScoreUpdated?.Invoke(CurrentScore);
+    }
+}
---
+++ Assets/Scripts/Presentation/Game/ScoreboardView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Presentation.Game
+{
+    public class ScoreboardView : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _scoreText;
+        
+        private void Awake()
+        {
+            GameScoreboard.Instance.OnScoreUpdated += UpdateScore;
+        }
+
+        private void UpdateScore(int score)
+        {
+            _scoreText.SetText(score.ToString());
+        }
+    }
+}
