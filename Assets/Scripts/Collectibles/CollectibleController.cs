67cf49ee01cb22951c62f42ad504a6e1a317c6a3
Manu Diaz <mdiaz.sanguino@gmail.com>
2021-01-31 20:15:28 +0100

    Collectible items can be collect again

--- Assets/Scripts/Collectibles/CollectibleController.cs
+++ Assets/Scripts/Collectibles/CollectibleController.cs
@@ -1,19 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Collectibles
 {
     public class CollectibleController : MonoBehaviour
     {
-        [SerializeField] private CollectibleConfig _collectibleConfig;
-
+        public event Action<CollectibleController, float> OnCollected;
         public Collectible.CollectibleType Type => _collectibleConfig.Collectible.Type;
+        
+        [SerializeField] private CollectibleConfig _collectibleConfig;
 
         public Collectible Collect()
         {
             ActivateGameObject(false);
-            var respawnTime = GetRespawnTime();
-            Invoke(nameof(Spawn), respawnTime);
-            
+            OnCollected?.Invoke(this, GetRespawnTime());
             return _collectibleConfig.Collectible;
         }
 
@@ -22,15 +23,15 @@ namespace Collectibles
             ActivateGameObject(true);
         }
         
-        public float GetRespawnTime()
-        {
-            var respawnInfo = _collectibleConfig.Collectible.RespawnTime;
-            return Random.Range(respawnInfo.MinTime, respawnInfo.MaxTime);
-        }
-
         private void ActivateGameObject(bool isActive)
         {
             gameObject.SetActive(isActive);
+        }
+        
+        private float GetRespawnTime()
+        {
+            var respawnInfo = _collectibleConfig.Collectible.RespawnTime;
+            return Random.Range(respawnInfo.MinTime, respawnInfo.MaxTime);
         }
     }
 }
---
+++ Assets/Scripts/Collectibles/Controllers/ACollectibleController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Collectibles.Controllers
+{
+    public abstract class ACollectibleController : MonoBehaviour, ICollectible
+    {
+        public event Action<ACollectibleController> OnSpawnPointIsFree;
+        public Collectible.CollectibleType Type => _collectibleConfig.Collectible.Type;
+        
+        [SerializeField] protected CollectibleConfig _collectibleConfig;
+
+        public float GetRespawnTime()
+        {
+            var respawnInfo = _collectibleConfig.Collectible.RespawnTime;
+            return Random.Range(respawnInfo.MinTime, respawnInfo.MaxTime);
+        }
+
+        public int Collect()
+        {
+            ActivateGameObject(false);
+            var respawnTime = GetRespawnTime();
+            Invoke(nameof(CanBeRespawned), respawnTime);
+            return _collectibleConfig.Collectible.Score;
+        }
+
+        public void Spawn()
+        {
+            ActivateGameObject(true);
+        }
+        
+        protected void ActivateGameObject(bool isActive)
+        {
+            gameObject.SetActive(isActive);
+        }
+
+        protected void CanBeRespawned()
+        {
+            OnSpawnPointIsFree?.Invoke(this);
+        }
+    }
+}
