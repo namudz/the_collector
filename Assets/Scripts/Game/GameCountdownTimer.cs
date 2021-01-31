7967d33e653a56a158b5d7b78a250643239f5427
Manu Diaz <mdiaz.sanguino@gmail.com>
2021-01-31 13:40:06 +0100

    Updated countown timer to use level config

--- Assets/Scripts/Game/GameCountdownTimer.cs
+++ Assets/Scripts/Game/GameCountdownTimer.cs
@@ -6,26 +6,31 @@ public class GameCountdownTimer : IGameCountdownTimer
     public event Action<float> OnCountdownUpdated;
     public event Action OnCountdownFinished;
 
-    private const float InitialCountdown = 100f;
+    private float _initialCountdown;
     private float _currentCountdown;
     private float _startTime;
+    
+    public void SetInitialCountdown(float countdown)
+    {
+        _initialCountdown = countdown;
+    }
 
     public void StartCountdown()
     {
         _startTime = Time.time;
-        _currentCountdown = InitialCountdown;
+        _currentCountdown = _initialCountdown;
         LaunchCountdownUpdated();
     }
-    
+
     public void Reset()
     {
-        _currentCountdown = InitialCountdown;
+        _currentCountdown = _initialCountdown;
     }
 
     public void UpdateCountdown()
     {
         var elapsedTime = Time.time - _startTime;
-        _currentCountdown = InitialCountdown - elapsedTime;
+        _currentCountdown = _initialCountdown - elapsedTime;
         if (_currentCountdown <= 0f)
         {
             _currentCountdown = 0f;
--- Assets/Scripts/Game/IGameCountdownTimer.cs
+++ Assets/Scripts/Game/IGameCountdownTimer.cs
@@ -5,6 +5,7 @@ public interface IGameCountdownTimer
     event Action<float> OnCountdownUpdated;
     event Action OnCountdownFinished;
     
+    void SetInitialCountdown(float currentLevelCountdown);
     void StartCountdown();
     void UpdateCountdown();
     void Reset();
